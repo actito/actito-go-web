3413dd835c9d268a93d6d030da2289fd1c40a6e9
rubensantos <rubensantos994@hotmail.com>
2025-11-27 18:12:23 +0000

    chore: update third-party dependencies
    Additional notes:
    - Added babel configuration in ember-cli-build.js because of ember-concurrency (mandatory since v5)
    - updated Font awesome configuration following the respective upgrading guide: https://github.com/FortAwesome/ember-fontawesome/blob/3.x/UPGRADING.md
    - Removed ember-flatpickr, ember-power-select and ace-builds dependencies, since they were not being used

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@ import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
+import './font-awesome';
 
 export default class App extends Application {
   ready() {
---
+++ app/font-awesome.js
@@ -0,0 +1,9 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { config } from '@fortawesome/fontawesome-svg-core';
+import * as freeSolidSvgIcons from '@fortawesome/free-solid-svg-icons';
+import * as freeBrandSvgIcons from '@fortawesome/free-brands-svg-icons';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+
+config.autoAddCss = false;
+
+library.add(freeSolidSvgIcons['fas'], freeBrandSvgIcons['fab']);
