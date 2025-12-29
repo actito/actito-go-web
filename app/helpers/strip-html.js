05bd2a65f01f515433e418d3a2460d384b980d2c
rubensantos <rubensantos994@hotmail.com>
2025-12-29 15:34:54 +0000

    feat: allow line breaks in the product details description

--- app/helpers/strip-html.js
+++ app/helpers/strip-html.js
@@ -4,5 +4,10 @@ export default helper(function stripHtml([htmlString]) {
   if (!htmlString) return '';
 
   const doc = new DOMParser().parseFromString(htmlString, 'text/html');
+
+  doc.querySelectorAll('br').forEach((br) => {
+    br.replaceWith('\n');
+  });
+
   return doc.body.textContent || '';
 });
